dccea6b4c16a4c1c8204a522ad41cf0b44f938ba
dzwdz <kg67199@gmail.com>
2021-06-11 23:15:06 +0200

    Combine find_group and add_group, some other minor changes

--- main.c
+++ main.c
@@ -9,12 +9,15 @@
 
 #include "config.h"
 
-_Noreturn void die(const char *err, ...);
-gid_t find_group(const char *name);
-void add_group(gid_t gid);
-
 gid_t groups[NGROUPS_MAX];
 int groups_amt;
+
+
+_Noreturn void die(const char *err, ...);
+
+// finds a group by name and pushes it to the end of groups[]
+void push_group(const char *name);
+
 
 _Noreturn void die(const char *err, ...) {
 	va_list argp;
@@ -25,30 +28,30 @@ _Noreturn void die(const char *err, ...) {
 	exit(1);
 }
 
-gid_t find_group(const char *name) {
-	FILE *fp = fopen("/etc/group", "r");
-	if (fp == NULL) die("couldn't open /etc/group\n");
-
+void push_group(const char *name) {
+	FILE *fp;
 	char *line = NULL, *token = NULL;
 	size_t len = 0;
 	ssize_t read;
+
+	if (groups_amt > NGROUPS_MAX)
+		die("too many groups\n");
+
+	fp = fopen("/etc/group", "r");
+	if (fp == NULL)
+		die("couldn't open /etc/group\n");
 
 	while ((read = getline(&line, &len, fp)) != -1) {
 		token = strtok(line, ":");
 		if (strcmp(token, name) == 0) {
 			token = strtok(NULL, ":");
 			token = strtok(NULL, ":");
-			return atoi(token);
+			groups[groups_amt++] = atoi(token);
+			return;
 		}
 	}
 
 	die("unknown group: %s\n", name);
-}
-
-void add_group(gid_t gid) {
-	if (groups_amt > NGROUPS_MAX) die("too many groups\n");
-
-	groups[groups_amt++] = gid;
 }
 
 int main(int argc, char *const *argv) {
@@ -64,10 +67,12 @@ int main(int argc, char *const *argv) {
 
 	for (int i = 0; perms[i].exe != NULL; i++)
 		if (strcmp(perms[i].exe, argv[1]) == 0)
-			add_group(find_group(perms[i].group));
+			push_group(perms[i].group);
 
-	if (setgroups(groups_amt, groups) == -1)
-		die("setgroups() call failed: %s.\nis the binary privileged?\n", strerror(errno));
+	if (setgroups(groups_amt, groups) == -1) {
+		die("covert: Unable to set the supplementary GIDs. "
+		    "Is the binary privileged?");
+	}
 
 	execv(argv[1], &argv[2]);
 
