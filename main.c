b54674a1ccbc12bfe0c2ed26b4329f194bc1ad98
dzwdz <kg67199@gmail.com>
2021-06-12 13:09:03 +0200

    Allow specifying which groups are able to use covert

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <grp.h>
 #include <limits.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,12 +11,20 @@
 #define CONFIG_PATH "/etc/covert"
 
 gid_t groups[NGROUPS_MAX];
-int groups_amt;
+int og_groupamt, new_groupamt;
 
+bool is_in_group(gid_t gid);
 gid_t find_group(const char *name);
 void push_group(gid_t gid);
 void apply_config();
 
+
+bool is_in_group(gid_t gid) {
+	for (int i = 0; i < og_groupamt; i++) {
+		if (groups[i] == gid) return true;
+	}
+	return false;
+}
 
 gid_t find_group(const char *name) {
 	struct group *grp = getgrnam(name);
@@ -24,35 +33,57 @@ gid_t find_group(const char *name) {
 }
 
 void push_group(gid_t gid) {
-	if (groups_amt >= NGROUPS_MAX)
+	if (new_groupamt >= NGROUPS_MAX)
 		DIE("you've hit the group limit\n");
 
-	groups[groups_amt++] = gid;
+	groups[new_groupamt++] = gid;
 }
 
 void apply_config(const char *program) {
 	FILE *fp;
 	char *line = NULL, *column = NULL, *group = NULL;
 	char *saveptr1, *saveptr2;
-	size_t len = 0;
+	size_t len = 0, line_no = 0;
 	ssize_t read;
+	bool allowed;
 
 	if (!(fp = fopen(CONFIG_PATH, "r")))
 		DIE("couldn't read " CONFIG_PATH "\n");
 
 	while ((read = getline(&line, &len, fp)) != -1) {
+		line_no++;
+
+		// first column - program name
 		column = strtok_r(line, " \t\n", &saveptr1);
 		if (!column || column[0] == '#') continue;
+		if (strcmp(column, program) != 0) continue;
 
-		if (strcmp(column, program) == 0) {
-			column = strtok_r(NULL, " \t\n", &saveptr1);
-			if (!column) continue; // TODO warn about misconfiguration
+		// second column - groups that are allowed to elevate themselves
+		column = strtok_r(NULL, " \t\n", &saveptr1);
+		if (!column)
+			DIE("syntax error @ %s:%ld\n", CONFIG_PATH, line_no);
 
-			group = strtok_r(column, ",", &saveptr2);
-			while (group != NULL) {
-				push_group(find_group(group));
-				group = strtok_r(NULL, ",", &saveptr2);
+		allowed = false;
+		group = strtok_r(column, ",", &saveptr2);
+		while (group != NULL) {
+			if (*group == '*' || is_in_group(find_group(group))) {
+				printf("%s\n", group);
+				allowed = true;
+				break;
 			}
+			group = strtok_r(NULL, ",", &saveptr2);
+		}
+		if (!allowed) continue;
+
+		// third column - groups to add to the program
+		column = strtok_r(NULL, " \t\n", &saveptr1);
+		if (!column)
+			DIE("syntax error @ %s:%ld\n", CONFIG_PATH, line_no);
+
+		group = strtok_r(column, ",", &saveptr2);
+		while (group != NULL) {
+			push_group(find_group(group));
+			group = strtok_r(NULL, ",", &saveptr2);
 		}
 	}
 
@@ -66,12 +97,13 @@ int main(int argc, char *const *argv) {
 		    "intended to be run directly by end users.\n");
 	}
 
-	if ((groups_amt = getgroups(NGROUPS_MAX, groups)) < 0)
+	if ((og_groupamt = getgroups(NGROUPS_MAX, groups)) < 0)
 		DIE("getgroups() call failed\n");
+	new_groupamt = og_groupamt;
 	
 	apply_config(argv[1]);
 
-	if (setgroups(groups_amt, groups) < 0) {
+	if (setgroups(new_groupamt, groups) < 0) {
 		DIE("covert: Unable to set the supplementary GIDs. "
 		    "Is the binary privileged?\n");
 	}
