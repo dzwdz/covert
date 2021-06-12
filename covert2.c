578b279a00fd355d254d90018f9dc324547c9dc3
dzwdz <kg67199@gmail.com>
2021-06-12 14:05:29 +0200

    Separate the rule parsing into a new function

--- covert2.c
+++ covert2.c
@@ -9,6 +9,7 @@
 #define DIE(...) {fprintf(stderr, __VA_ARGS__); exit(1);}
 
 #define CONFIG_PATH "/etc/covert"
+#define WHITESPACE " \t\n"
 
 gid_t groups[NGROUPS_MAX];
 int og_groupamt, new_groupamt;
@@ -16,6 +17,7 @@ int og_groupamt, new_groupamt;
 bool is_in_group(gid_t gid);
 gid_t find_group(const char *name);
 void push_group(gid_t gid);
+void apply_rule(char *rule, const char *program, size_t line_no);
 void apply_config();
 
 
@@ -39,50 +41,52 @@ void push_group(gid_t gid) {
 	groups[new_groupamt++] = gid;
 }
 
+void apply_rule(char *rule, const char *program, size_t line_no) {
+	bool privileged = false;
+	char *column = NULL, *group = NULL;
+	char *sp1, *sp2;
+
+	// first column - program name
+	column = strtok_r(rule, WHITESPACE, &sp1);
+	if (!column || column[0] == '#') return;
+	if (strcmp(column, program) != 0) return;
+
+	// second column - groups that are allowed to elevate themselves
+	column = strtok_r(NULL, WHITESPACE, &sp1);
+	if (!column)
+		DIE("syntax error @ %s:%ld\n", CONFIG_PATH, line_no);
+
+	group = strtok_r(column, ",", &sp2);
+	while (group != NULL && !privileged) {
+		privileged |= (*group == '*') // * is a catchall "group"
+		           || is_in_group(find_group(group));
+
+		group = strtok_r(NULL, ",", &sp2);
+	}
+	if (!privileged) return;
+
+	// third column - groups to add to the program
+	column = strtok_r(NULL, WHITESPACE, &sp1);
+	if (!column)
+		DIE("syntax error @ %s:%ld\n", CONFIG_PATH, line_no);
+
+	group = strtok_r(column, ",", &sp2);
+	while (group != NULL) {
+		push_group(find_group(group));
+		group = strtok_r(NULL, ",", &sp2);
+	}
+}
+
 void apply_config(const char *program) {
 	FILE *fp;
-	char *line = NULL, *column = NULL, *group = NULL;
-	char *saveptr1, *saveptr2;
-	size_t len = 0, line_no = 0;
-	ssize_t read;
+	char *buf = NULL;
+	size_t buflen = 0, line_no = 0;
 
 	if (!(fp = fopen(CONFIG_PATH, "r")))
 		DIE("couldn't read " CONFIG_PATH "\n");
 
-	while ((read = getline(&line, &len, fp)) != -1) {
-		line_no++;
-
-		// first column - program name
-		column = strtok_r(line, " \t\n", &saveptr1);
-		if (!column || column[0] == '#') continue;
-		if (strcmp(column, program) != 0) continue;
-
-		// second column - groups that are allowed to elevate themselves
-		column = strtok_r(NULL, " \t\n", &saveptr1);
-		if (!column)
-			DIE("syntax error @ %s:%ld\n", CONFIG_PATH, line_no);
-
-		group = strtok_r(column, ",", &saveptr2);
-		while (group != NULL) {
-			if (*group == '*' || is_in_group(find_group(group)))
-				goto allowed;
-
-			group = strtok_r(NULL, ",", &saveptr2);
-		}
-		continue;
-
-		// third column - groups to add to the program
-allowed:
-		column = strtok_r(NULL, " \t\n", &saveptr1);
-		if (!column)
-			DIE("syntax error @ %s:%ld\n", CONFIG_PATH, line_no);
-
-		group = strtok_r(column, ",", &saveptr2);
-		while (group != NULL) {
-			push_group(find_group(group));
-			group = strtok_r(NULL, ",", &saveptr2);
-		}
-	}
+	while (getline(&buf, &buflen, fp) != -1)
+		apply_rule(buf, program, ++line_no);
 
 	fclose(fp);
 }
